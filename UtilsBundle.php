cb068a28eac4c397aa96ce587795bc0f2cecff2c
Alain Bangoula <alain@come-n-see.fr>
2012-10-10 01:36:05 +0200

    bundling

---
+++ UtilsBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CNSEE\UtilsBundle;
+
+
+/**
+ * @author Lukas Kahwe Smith <smith@pooteeweet.org>
+ * @author Eriksen Costa <eriksencosta@gmail.com>
+ */
+class CNSEEUtilsBundle extends Bundle
+{
+    
+}
