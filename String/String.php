8ffa57945e5b9e247997f3c9d28e74cf9ce4bd46
Alain Bangoula <alain.bangoula@come-n-see.fr>
2013-10-09 16:51:46 +0200

    Update String.php

--- String/String.php
+++ String/String.php
@@ -116,6 +116,24 @@ class String{
             return preg_replace("/\<br\s*\/?\>/i", "\r\r", $text);
         }
 
- 
+        static function slugify($string)
+        {
+            // Remove accents from characters
+            $string = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+
+            // Everything lowercase
+            $string = strtolower($string);
+
+            // Replace all non-word characters by dashes
+            $string = preg_replace("/\W/", "-", $string);
+
+            // Replace double dashes by single dashes
+            $string = preg_replace("/-+/", '-', $string);
+
+            // Trim dashes from the beginning and end of string
+            $string = trim($string, '-');
+
+            return $string;
+        }
 
 }
